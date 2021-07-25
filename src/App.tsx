d8791af71e6a14523cf36461798c3a9c31f7633f
polarathene <5098581+polarathene@users.noreply.github.com>
2021-07-25 20:24:00 +1200

    Add basic form inputs

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,49 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Hello :)
+      <form>
+        <div>
+          <label htmlFor="cc_number">Card Number:</label>
+          <input
+            id="cc_number"
+            type="number"
+            required
+          />
+        </div>
+
+        <fieldset>
+          <legend>Expiry Date:</legend>
+          <input
+            id="cc_expiry_month"
+            type="number"
+            min="1"
+            max="12"
+            placeholder="MM"
+            required
+          />
+          <span>/</span>
+          <input
+            id="cc_expiry_year"
+            type="number"
+            min="21"
+            max="99"
+            placeholder="YY"
+            required
+          />
+        </fieldset>
+
+        <div>
+          <label htmlFor="cc_code">CVC:</label>
+          <input
+            id="cc_code"
+            type="number"
+            placeholder="123"
+            required
+          />
+        </div>
+
+        <button type="submit">Submit</button>
+      </form>
     </div>
   )
 }
