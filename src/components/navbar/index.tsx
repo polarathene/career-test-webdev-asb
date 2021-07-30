87af259748493d9f98778273e720afb9174ca53c
polarathene <5098581+polarathene@users.noreply.github.com>
2021-07-30 13:41:31 +1200

    Use SVG Icons for menu button toggle

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { FaHamburger, FaArrowLeft } from 'react-icons/fa';
 import styles from './styles.module.scss'
 
 export const NavMenu: React.FC<HTMLProps> = () => {
@@ -10,7 +11,10 @@ export const NavMenu: React.FC<HTMLProps> = () => {
       <h1>{isOpen ? "Menu" : "Register Card Form"}</h1>
       <nav className={styles.navbar}>
         <button onClick={toggleMenu}>
-          {isOpen ? "<=" : "Open"}
+        {isOpen
+          ? <FaArrowLeft aria-hidden="true" />
+          : <FaHamburger aria-hidden="true" />
+        }
         </button>
         <div style={{display: isOpen ? "block" : "none" }}>
           <p>This is menu content.</p>
